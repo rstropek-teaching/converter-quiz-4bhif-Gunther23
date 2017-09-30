c3406a1b811d26bf4a23ab3121997a713a5a8525
Gunther23 <guenther.julian@gmx.at>
2017-09-30 18:19:49 +0200

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,1 +1,57 @@
-console.log('Not implemented yet!');
+
+var value = "";
+var source = "";
+var target = "";
+var result;
+
+const error = "Invalid Operation";
+
+const readline = require('readline');
+const console = require('console');
+
+    value = process.argv[2];
+    source = process.argv[3];
+    target = process.argv[5];
+
+    if(source == "m" && target == "cm"){
+        result = parseInt(value)*100;
+        console.log(result);
+    }
+
+    else if(source == "m" && target == "mm"){
+        result = parseInt(value)*1000;
+        console.log(result);
+    }
+
+    else if(source == "cm" && target == "mm"){
+        result = parseInt(value)*10;
+        console.log(result);
+    }
+
+    else if(source == "cm" && target == "m"){
+        result = parseInt(value)*0.01;
+        console.log(result);
+    }
+
+    else if(source == "mm" && target == "cm"){
+        result = parseInt(value)*0.1;
+        console.log(result);
+    }
+
+    else if(source == "mm" && target == "m"){
+        result = parseInt(value)*0.001;
+        console.log(result);
+    }
+    else if(source == "g" && target == "kg"){
+        result = parseInt(value)*0.001;
+        console.log(result);
+    }
+    else if(source == "kg" && target == "g"){
+        result = parseInt(value)*1000;
+        console.log(result);
+    }
+    else{
+        console.log(error);
+    }
+
+    
